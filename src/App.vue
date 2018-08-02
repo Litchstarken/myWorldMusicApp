c3e4674529cb7ebc1def498ae1f6c95124364766
John Jairo Castillo Cortes <john.castillo@globant.com>
2018-08-02 12:39:17 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+    #app
+      img(src='./assets/logo2.png')
+      h1 What's world is listening on lastfm?
+      select(v-model="selectedCountry")  
+        option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
+      spinner(v-show="loading") 
+      ul
+        artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+      
+</template>
+
+<script>
+  import artist from './components/artist.vue'
+  import spinner from './components/spinner.vue'
+  import getArtist from './api/index.js'
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries:[
+      {name: 'Colombia', value:'colombia'},
+      {name: 'Argentina', value:'argentina'},
+      {name: 'Canada', value:'canada'},
+      {name: 'Poland', value:'poland'},
+      {name: 'Germany', value:'germany'}
+      ],
+      selectedCountry: 'colombia',
+      loading: true
+    }
+  },
+  components: {
+    artist,
+    spinner
+  },
+  methods: {
+    refreshArtist(){
+      const self = this
+      this.loading = true
+      this.artists = []
+      getArtist(this.selectedCountry)
+      .then(function (artists){
+        self.loading = false
+        self.artists = artists
+      })
+    }
+  },
+  mounted() {
+    this.refreshArtist()
+  },
+  watch: {
+    selectedCountry(){
+      this.refreshArtist()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    margin-top 60px
+
+  h1, h2
+    font-weight normal
+
+  ul
+    list-style-type none
+    padding 0
+
+  li
+    display inline-block
+    margin 0 10px
+
+  a
+    color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+export default{
+	appname: 'myPlatziMusic',
+	apiKey: '74f0a591dad5ae1d63893b9980aed8d3',
+	secret: '22a5ecab0406a8855884f085b8384d13',
+	registeredTo: 'Zaitsenig'
+}
+
+
+
+
+// Application name	myPlatziMusic
+// API key	74f0a591dad5ae1d63893b9980aed8d3
+// Shared secret	22a5ecab0406a8855884f085b8384d13
+// Registered to	Zaitsenig
